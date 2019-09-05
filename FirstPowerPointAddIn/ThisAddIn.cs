b2e892bdcbb8d031d554dd8f333dffa4e2d863bb
sonegishi <5055ngs@gmail.com>
2019-09-05 14:03:10 -0400

    Add a text to every new slide

--- FirstPowerPointAddIn/ThisAddIn.cs
+++ FirstPowerPointAddIn/ThisAddIn.cs
@@ -12,10 +12,20 @@ namespace FirstPowerPointAddIn
     {
         private void ThisAddIn_Startup(object sender, System.EventArgs e)
         {
+            this.Application.PresentationNewSlide +=
+                new PowerPoint.EApplication_PresentationNewSlideEventHandler(
+                    Application_PresentationNewSlide);
         }
 
         private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
         {
+        }
+
+        void Application_PresentationNewSlide(PowerPoint.Slide Sld)
+        {
+            PowerPoint.Shape textBox = Sld.Shapes.AddTextbox(
+                Office.MsoTextOrientation.msoTextOrientationHorizontal, 0, 0, 500, 50);
+            textBox.TextFrame.TextRange.InsertAfter("This text was added by using code.");
         }
 
         #region VSTO generated code
